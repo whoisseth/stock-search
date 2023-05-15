4db4bcec279ec00ee5ef75f0dd8898b3465eb3e1
utkarsh <55540613+whoisseth@users.noreply.github.com>
2023-05-15 22:52:39 +0530

    fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 /** @format */
 
-'use client'
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
-const queryClient = new QueryClient();
 
 export const metadata = {
   title: "Create Next App",
@@ -20,11 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
